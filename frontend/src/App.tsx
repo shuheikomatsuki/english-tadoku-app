12f3ed5f395c3a3e6566d0c3cf4337c4e3202356
shuheikomatsuki <shuheijy@gmail.com>
2025-09-13 23:08:18 +0900

    frontendのプロジェクト初期化

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="p-6 bg-blue-500 text-white rounded-xl">
+      <h1 className="text-2xl font-bold">Hello Tailwind v4 + Vite!</h1>
+    </div>
+  )
+}
+
+export default App
