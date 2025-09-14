ed443881dc7b5a3fc035db52fc8051103d3ad3e4
shuheikomatsuki <shuheijy@gmail.com>
2025-09-14 12:52:10 +0900

    model/story.go, model/user.goにdbタグを追加

--- backend/internal/model/story.go
+++ backend/internal/model/story.go
@@ -5,10 +5,10 @@ import (
 )
 
 type Story struct {
-	ID int `json:"id"`
-	UserID int `json:"user_id"`
-	Title string `json:"title"`
-	Content string `json:"content"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        int 		`json:"id"         db:"id"`
+	UserID    int  		`json:"user_id"    db:"user_id"`
+	Title     string 	`json:"title"      db:"title"`
+	Content   string 	`json:"content"    db:"content"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
