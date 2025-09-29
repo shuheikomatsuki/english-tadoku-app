297596f3341e59a31c8a5fa1505e7ba9c078065e
shuheikomatsuki <shuheijy@gmail.com>
2025-09-29 17:49:38 +0900

    不要なコメントアウトを削除

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,30 +26,6 @@ function App() {
         )}
       </Route>
     </Routes>
-
-    // <div className="min-h-screen bg-gray-100">
-    //   <div className="container mx-auto p-4 py-10">
-    //     <h1 className="text-4xl font-bold text-gray-800 mb-8 text-center">
-    //       English Tadoku App
-    //     </h1>
-    //     {/* {isAuthenticated ? <Dashboard /> : <AuthPage />} */}
-    //     <Routes>
-    //       {!isAuthenticated && (
-    //         <>
-    //           <Route path="/auth" element={<AuthPage />} />
-    //           <Route path="*" element={<Navigate to="/auth" replace />} />
-    //         </>
-    //       )}
-    //       {isAuthenticated && (
-    //         <>
-    //           <Route path="/" element={<Dashboard />} />
-    //           <Route path="/stories/:id" element={<StoryDetail />} />
-    //           <Route path="*" element={<Navigate to="/" replace />} />
-    //         </>
-    //       )}
-    //     </Routes>
-    //   </div>
-    // </div>
   );
 };
 
