00895316a4fee8013e6fa85f6918a67aeb57154b
shuheikomatsuki <shuheijy@gmail.com>
2025-11-02 21:21:47 +0900

    apiBaseUrlを.envから読み取るようにした

--- frontend/src/apiClient.ts
+++ frontend/src/apiClient.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
+
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8080/api/v1',
+    baseURL: `${apiBaseUrl}/api/v1`,
     headers: {
         'Content-Type': 'application/json',
     },
